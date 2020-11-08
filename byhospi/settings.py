491581412037ea3d8a4425d5b86b49e55f1aee59
Yauheni <e.subbota@syberry.com>
2020-11-08 22:52:24 +0300

    edited pre-comit config file

--- byhospi/settings.py
+++ byhospi/settings.py
@@ -2,6 +2,7 @@ import os
 
 import dj_database_url
 from django.contrib.staticfiles import finders
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
