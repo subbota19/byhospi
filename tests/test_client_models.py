55fef53e14c3dc653aaca17cb9b815dbea4eb19f
Yauheni <e.subbota@syberry.com>
2020-11-17 01:01:12 +0300

    create test with paramatrezation

--- tests/test_client_models.py
+++ tests/test_client_models.py
@@ -1,61 +1,30 @@
-from abc import ABC
-from abc import abstractmethod
-
 from django.test import TestCase
+from parameterized import parameterized_class
 
 from client.models import Client
 from client.models import HosAdmin
 
 MAX_USERNAME_LENGTH = 20
 MIN_USERNAME_LENGTH = 4
-
 MIN_PASSWORD_LENGTH = 4
 
-USERNAME = "yauheni"
-PASSWORD = "zs1919"
-EMAIL = "zhenya@mail.ru"
 
-
-class UserTest(ABC):
-    @abstractmethod
-    def test_validate_obj_representation(self):
-        pass
-
-    @abstractmethod
-    def test_validate_username(self):
-        pass
-
-    @abstractmethod
-    def test_check_username_length(self):
-        pass
-
-    @abstractmethod
-    def test_validate_password(self):
-        pass
-
-    @abstractmethod
-    def test_check_password_length(self):
-        pass
-
-    @abstractmethod
-    def is_admin_user(self):
-        pass
-
-    @staticmethod
-    def get_testing_user(model):
-        return model.objects.get(username=USERNAME)
-
-
-class ClientModelTest(TestCase, UserTest):
+@parameterized_class(
+    ("username", "password", "email"),
+    [("yauheni", "zs1919", "zhenya@mail.ru"), ("anton", "an1234ton", "anto@mail.com")],
+)
+class ClientModelTest(TestCase):
     def setUp(self):
-        Client.objects.create(username=USERNAME, password=PASSWORD, email=EMAIL)
-        self.test_user = self.get_testing_user(Client)
+        Client.objects.create(
+            username=self.username, password=self.password, email=self.email
+        )
+        self.test_user = self.get_testing_user()
 
     def test_validate_obj_representation(self):
-        self.assertEquals(self.test_user.__str__(), USERNAME)
+        self.assertEquals(self.test_user.__str__(), self.username)
 
     def test_validate_username(self):
-        self.assertEquals(self.test_user.username, USERNAME)
+        self.assertEquals(self.test_user.username, self.username)
 
     def test_check_username_length(self):
         self.assertGreaterEqual(
@@ -63,25 +32,34 @@ class ClientModelTest(TestCase, UserTest):
         ) and self.assertLessEqual(len(self.test_user.username), MAX_USERNAME_LENGTH)
 
     def test_validate_password(self):
-        self.assertEquals(self.test_user.password, PASSWORD)
+        self.assertEquals(self.test_user.password, self.password)
 
     def test_check_password_length(self):
         self.assertGreaterEqual(len(self.test_user.password), MIN_PASSWORD_LENGTH)
 
-    def is_admin_user(self):
+    def test_is_admin_user(self):
         self.assertEquals(self.test_user.is_admin, False)
 
+    def get_testing_user(self):
+        return Client.objects.get(username=self.username)
 
-class HosAdminModelTest(TestCase, UserTest):
+
+@parameterized_class(
+    ("username", "password", "email"),
+    [("ivan", "67iv49", "ivan@mail.ru"), ("den1", "den1den1", "den@mail.com")],
+)
+class HosAdminModelTest(TestCase):
     def setUp(self):
-        HosAdmin.objects.create(username=USERNAME, password=PASSWORD, email=EMAIL)
-        self.test_user = self.get_testing_user(HosAdmin)
+        HosAdmin.objects.create(
+            username=self.username, password=self.password, email=self.email
+        )
+        self.test_user = self.get_testing_user()
 
     def test_validate_obj_representation(self):
-        self.assertEquals(self.test_user.__str__(), USERNAME)
+        self.assertEquals(self.test_user.__str__(), self.username)
 
     def test_validate_username(self):
-        self.assertEquals(self.test_user.username, USERNAME)
+        self.assertEquals(self.test_user.username, self.username)
 
     def test_check_username_length(self):
         self.assertGreaterEqual(
@@ -89,10 +67,13 @@ class HosAdminModelTest(TestCase, UserTest):
         ) and self.assertLessEqual(len(self.test_user.username), MAX_USERNAME_LENGTH)
 
     def test_validate_password(self):
-        self.assertEquals(self.test_user.password, PASSWORD)
+        self.assertEquals(self.test_user.password, self.password)
 
     def test_check_password_length(self):
         self.assertGreaterEqual(len(self.test_user.password), MIN_PASSWORD_LENGTH)
 
     def is_admin_user(self):
         self.assertEquals(self.test_user.is_admin, True)
+
+    def get_testing_user(self):
+        return HosAdmin.objects.get(username=self.username)
